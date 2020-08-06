855b23d0a12f23b3c1af44a046ab11b1e817b72f
BLZbanme <swordcome@gmail.com>
2020-08-06 13:48:12 +0800

    finish 1.0

--- toy-vue.js
+++ toy-vue.js
@@ -1,3 +1,182 @@
+const utils = {
+
+    getValue(expr, vm) {
+        return vm.$data[expr.trim()];
+    },
+
+    setValue(expr, vm, newValue) {
+        vm.$data[expr.trim()] = newValue;
+    },
+
+    model(node, value, vm) {
+        const initValue = this.getValue(value, vm);
+        new Watcher(value, vm, (newValue) => {
+            this.modelUpdater(node, newValue);
+        })
+
+        node.addEventListener('input', e => {
+            const newValue = e.target.value;
+            this.setValue(value, vm, newValue);
+        })
+
+        this.modelUpdater(node, initValue);
+    },
+
+    text(node, value, vm) {
+        let result;
+        if (value.includes("{{")) {
+            result = value.replace(/\{\{(.+)\}\}/g, (...args) => {
+                const expr = args[1].trim()
+                new Watcher(expr, vm, (newVal) => {
+                    this.textUpdater(node, newVal);
+                });
+                return this.getValue(args[1], vm);
+            })
+        }
+        else {
+            // v-text='xxx'
+            result = this.getValue(value, vm);
+        }
+        this.textUpdater(node, result);
+    },
+
+    on(node, value, vm, eventName) {
+        const fn = vm.$options.methods[value];
+        node.addEventListener(eventName, fn.bind(vm), false);
+    },
+
+    textUpdater(node, value) {
+        node.textContent = value;
+    },
+    
+    modelUpdater(node, value) {
+        node.value = value;
+    }
+}
+
+class Watcher {
+    constructor(expr, vm, cb) {
+        this.expr = expr;
+        this.vm = vm;
+        this.cb = cb;
+        // 通过getter的形式对数据进行绑定，标记当前的watcher
+        this.oldValue = this.getOldValue();
+    }
+
+    getOldValue() {
+        Dep.target = this;
+        const oldValue = utils.getValue(this.expr, this.vm);
+        Dep.target = null;
+        return oldValue;
+    }
+
+    update() {
+        const newValue = utils.getValue(this.expr, this.vm);
+        if (newValue !== this.oldValue) {
+            this.cb(newValue);
+        }
+    }
+}
+
+class Dep {
+    constructor() {
+        this.collect = [];
+    }
+
+    addWatcher(watcher) {
+        this.collect.push(watcher);
+    }
+
+    notify() {
+        this.collect.forEach(w => w.update());
+    }
+}
+
+
+class Compiler {
+    constructor(el, vm) {
+        this.el = this.isElementNode(el) ? el : document.querySelector(el);
+        this.vm = vm;
+
+        const fragment = this.compileFragment(this.el);
+
+        this.compile(fragment);
+        this.el.appendChild(fragment);
+    }
+
+    compile(fragment) {
+        const childNodes = Array.from(fragment.childNodes);
+        childNodes.forEach(childNode => {
+            if (this.isElementNode(childNode)) {
+                //标签结点h1 / input 读取属性，查看是否有v-开头的内容
+                
+                this.compileElement(childNode);
+            }
+            else if (this.isTextNode(childNode)) {
+                this.compileText(childNode);
+            }
+
+            if (childNode.childNodes && childNode.childNodes.length) {
+                this.compile(childNode);
+            }
+        })
+    }
+
+    compileElement(node) {
+        // v-model v-text v-on:click
+        const attributes = Array.from(node.attributes);
+        attributes.forEach(attr => {
+            const { name, value } = attr;
+            console.log('attr', name, value);
+            if (this.isDirector(name)) {
+                //指令 v-model v-text v-bind
+                const [, directive] = name.split('-');
+                const [compileKey, eventName] = directive.split(':');
+                utils[compileKey](node, value, this.vm, eventName);
+            }
+            else if (this.isEventName(name)) {
+                // @ 方法执行
+                const [, eventName] = name.split('@');
+                utils['on'](node, value, this.vm, eventName);
+            }
+        })
+    }
+
+    isDirector(name) {
+        return name.startsWith('v-');
+    }
+    
+    isEventName(name) {
+        return name.startsWith('@');
+    }
+
+    compileText(node) {
+        const content = node.textContent;
+        if (/\{\{.+\}\}/.test(content)) {
+            utils['text'](node, content, this.vm);
+        }
+    }
+
+    compileFragment(el) {
+        const f= document.createDocumentFragment();
+        let firstChild;
+        while (firstChild = el.firstChild) {
+            f.appendChild(firstChild);
+        }
+        
+        return f;
+    }
+
+    isElementNode(el) {
+        return el.nodeType === 1;
+    }
+
+    isTextNode(el) {
+        return el.nodeType === 3;
+    }
+}
+
+
 class Observer {
     constructor(data) {
         this.observe(data);
@@ -13,23 +192,26 @@ class Observer {
 
     defineReactive(obj, key, value) {
         this.observe(value);
+
+        const dep = new Dep();
+
         Object.defineProperty(obj, key, {
             get() {
-                console.log('$data get', key, value);
+                const target = Dep.target;
+                target && dep.addWatcher(target);
                 return value;
             },
             set: (newVal) => {
                 if (value === newVal) {
                     return;
                 }
-                console.log('$data set', key, newVal);
                 this.observe(newVal);
                 value = newVal;
+                dep.notify();
             }
         })
     }
 }
-
 
 class Vue {
 
@@ -40,6 +222,9 @@ class Vue {
 
         //触发 this.$data.xx 和模板的绑定
         new Observer(this.$data);
+
+        //处理模板部分，将模板中使用的data部分的变量和模板绑定起来
+        new Compiler(this.$el, this);
         this.proxyData(this.$data);
     }
 
--- 手写vue.md
+++ 手写vue.md
@@ -1,6 +1,6 @@
 # 手写vue
 
-
+![1596692677398](.\vue.png)
 
 ## 1.数据代理
 
@@ -83,6 +83,132 @@ class Observer {
                 value = newVal;
             }
         })
+    }
+}
+```
+
+## 3.模板编译
+
+对于element和text各自处理，"v-"和"{{  }}"
+
+```javascript
+class Compiler {
+    constructor(el, vm) {
+        this.el = this.isElementNode(el) ? el : document.querySelector(el);
+        this.vm = vm;
+
+        const fragment = this.compileFragment(this.el);
+
+        this.compile(fragment);
+        this.el.appendChild(fragment);
+    }
+
+    compile(fragment) {
+        const childNodes = Array.from(fragment.childNodes);
+        childNodes.forEach(childNode => {
+            if (this.isElementNode(childNode)) {
+                //标签结点h1 / input 读取属性，查看是否有v-开头的内容
+                
+                this.compileElement(childNode);
+            }
+            else if (this.isTextNode(childNode)) {
+                this.compileText(childNode);
+            }
+
+            if (childNode.childNodes && childNode.childNodes.length) {
+                this.compile(childNode);
+            }
+        })
+    }
+
+    compileElement(node) {
+        // v-model v-text v-on:click
+        const attributes = Array.from(node.attributes);
+        attributes.forEach(attr => {
+            const { name, value } = attr;
+            console.log('attr', name, value);
+            if (this.isDirector(name)) {
+                //指令 v-model v-text v-bind
+                const [, directive] = name.split('-');
+                const [compileKey, eventName] = directive.split(':');
+                utils[compileKey](node, value, this.vm, eventName);
+            }
+        })
+    }
+
+    isDirector(name) {
+        return name.startsWith('v-');
+    }
+
+    compileText(node) {
+        const content = node.textContent;
+        if (/\{\{.+\}\}/.test(content)) {
+            utils['text'](node, content, this.vm);
+        }
+    }
+
+    compileFragment(el) {
+        const f= document.createDocumentFragment();
+        let firstChild;
+        while (firstChild = el.firstChild) {
+            f.appendChild(firstChild);
+        }
+        
+        return f;
+    }
+
+    isElementNode(el) {
+        return el.nodeType === 1;
+    }
+
+    isTextNode(el) {
+        return el.nodeType === 3;
+    }
+}
+```
+
+## 4.依赖收集
+
+Dep构造函数，主要作用是收集Watcher,就是在模板编译，get的时候收集依赖，set的时候notify所有依赖。
+
+watcher的注册依赖的时候，最重要的就是```this.getOldValue();```，来把当前收集依赖的对象注册在一个全局变量中。
+
+```javascript
+class Dep {
+    constructor() {
+        this.collect = [];
+    }
+
+    addWatcher(watcher) {
+        this.collect.push(watcher);
+    }
+
+    notify() {
+        this.collect.forEach(w => w.update());
+    }
+}
+
+class Watcher {
+    constructor(expr, vm, cb) {
+        this.expr = expr;
+        this.vm = vm;
+        this.cb = cb;
+        // 通过getter的形式对数据进行绑定，标记当前的watcher
+        this.oldValue = this.getOldValue();
+    }
+
+    getOldValue() {
+        Dep.target = this;
+        const oldValue = utils.getValue(this.expr, this.vm);
+        Dep.target = null;
+        return oldValue;
+    }
+
+    update() {
+        const newValue = utils.getValue(this.expr, this.vm);
+        if (newValue !== this.oldValue) {
+            this.cb(newValue);
+        }
     }
 }
 ```
